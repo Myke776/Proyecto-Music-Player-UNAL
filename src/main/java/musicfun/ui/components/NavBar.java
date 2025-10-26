43a4d841257ac3391019771e7a0dd9ee8495c002
CciClo <tato.tandioy@gmail.com>
2025-10-25 20:28:12 -0500

    feat: seagrego funcionalidad a la navagacion

--- src/main/java/musicfun/ui/components/NavBar.java
+++ src/main/java/musicfun/ui/components/NavBar.java
@@ -2,6 +2,7 @@ package musicfun.ui.components;
 
 import java.util.List;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -21,7 +22,12 @@ public class NavBar extends VBox {
 		
 		super.getChildren().clear();
 		for (SceneInfo<?> sceneInfo : scenes) {
-			super.getChildren().add(this.createButton(sceneInfo.getIcon(), currentRute));
+			Button btn = this.createButton(sceneInfo, currentRute);
+			btn.setOnAction(e -> {
+				navigation.navigateTo(sceneInfo.getRouteName());
+				updateNavBar(navigation);
+			});
+			super.getChildren().add(btn);
 		}
 
 		super.setMaxHeight(Double.MIN_VALUE);
@@ -29,7 +35,17 @@ public class NavBar extends VBox {
 		super.getStyleClass().add("navbar");
 	}
 
-	private ImageView createButton(String routeImage, String routeName) {
+	private Button createButton(SceneInfo<?> sceneInfo, String currentRute) {
+		Button button = new Button();
+		button.setGraphic(this.createImage(sceneInfo.getIcon()));
+		button.getStyleClass().add("btn-nav");
+		if(currentRute.equals(sceneInfo.getRouteName())){
+			button.getStyleClass().add("btn-nav-active");
+		}
+		return button;
+	}
+
+	private ImageView createImage(String routeImage) {
 		Image image = new Image(getClass().getResourceAsStream(routeImage));
 		ImageView imageView= new ImageView(image);
 		imageView.setFitWidth(25);
